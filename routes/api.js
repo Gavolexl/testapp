c4f2d913d961f00db76bac5c3448526db018eceb
baylovbodia <1kn15b.baylov@gmail.com>
2018-01-17 12:20:02 +0200

    t
    t

--- routes/api.js
+++ routes/api.js
@@ -1,7 +1,28 @@
 var ObjectID = require('mongodb').ObjectID;
 module.exports = function(app, db) {
+    console.log("1");
 
-    app.get('/notes/:id', (req, res) => {
+    app.post('/api/posts', (req, res) => {
+        console.log("3");
+        db.collection("notes").find({}).toArray(function(err, result) {
+            if (err) throw err;
+            res.json({
+                posts: result
+            });
+        });
+    });
+
+    app.get('/', (req,res) => {
+        console.log("2");
+        res.render('index');
+    });
+    app.get('/partials/:name', (req, res) => {
+        var name = req.params.name;
+        res.render('partials/' + name);
+    });
+
+    app.get('/api/post/:id', (req, res) => {
+        console.log("111");
         const id = req.params.id;
         const details = { '_id': new ObjectID(id) };
         db.collection('notes').findOne(details, (err, item) => {
@@ -13,8 +34,12 @@ module.exports = function(app, db) {
         });
     });
 
-    app.post('/notes', (req, res) => {
-        const note = { text: req.body.body, title: req.body.title };
+
+
+
+    app.post('/api/post', (req, res) => {
+        console.log("4");
+        const note = { text: req.body.text, title: req.body.title };
         db.collection('notes').insert(note, (err, result) => {
             if (err) {
                 res.send({ 'error': 'An error has occurred' });
@@ -24,7 +49,7 @@ module.exports = function(app, db) {
         });
     });
 
-    app.delete('/notes/:id', (req, res) => {
+    app.delete('/api/post/:id', (req, res) => {
         const id = req.params.id;
         const details = { '_id': new ObjectID(id) };
         db.collection('notes').remove(details, (err, item) => {
@@ -36,7 +61,7 @@ module.exports = function(app, db) {
         });
     });
 
-    app.put ('/notes/:id', (req, res) => {
+    app.put ('/api/post/:id', (req, res) => {
         const id = req.params.id;
         const details = { '_id': new ObjectID(id) };
         const note = { text: req.body.body, title: req.body.title };
@@ -48,4 +73,9 @@ module.exports = function(app, db) {
             }
         });
     });
+
+    app.get('*', (req,res) => {
+        res.render('index');
+    });
+
 };
